a26341a5b151966478722361dc9a2b6630651ed8
Masashi YOSHIDA <152603719+MasaSky@users.noreply.github.com>
2024-01-23 10:05:25 +0900

    240113-01

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :genre
-	has_one_attached :image
-	has_many :cart_items
-	has_many :orders, through: :order_details
-	has_many :order_items
+  has_one_attached :image
+  has_many :cart_items
+  has_many :orders
+  has_many :order_items
 
   validates :genre_id, :name, :unit_price, presence: true
-	validates :introduction, length: { maximum: 200 }, presence: true
-	validates :unit_price, numericality: { only_integer: true }
+  validates :introduction, length: { maximum: 200 }, presence: true
+  validates :unit_price, numericality: { only_integer: true }
 
-	def self.looks(word)
-		@item = Item.where("name LIKE?","%#{word}%")
-	end
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name introduction]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
