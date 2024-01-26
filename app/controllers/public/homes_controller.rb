91a0368d8355666dff6cfa1aa1ac74938d8ded2a
MasaSky <152603719+MasaSky@users.noreply.github.com>
2024-01-26 07:16:35 +0000

    240126-01

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,10 @@
 class Public::HomesController < ApplicationController
   def top
-    @items = Item.all
+    @items = Item.all.limit(4).order("created_at DESC")
+    @genres = Genre.all
+  end
+
+  def about
+    @items = Item.all.limit(1).order("created_at DESC")
   end
 end
