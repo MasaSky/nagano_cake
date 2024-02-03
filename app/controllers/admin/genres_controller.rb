c9b23f582a972f7ee82ef28cabe80ab5cef96739
MasaSky <152603719+MasaSky@users.noreply.github.com>
2024-02-03 21:19:25 +0000

    240204-00

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,7 +7,6 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    @genre_all = Genre.where(id: id)
     if @genre.save
       flash[:notice] = "ジャンルを登録しました。"
       redirect_to admin_genres_path
